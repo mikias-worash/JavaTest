7f9e5f58dc45398661aa61190b281c7485e62595
Mikias <mikias.worash.a@gmail.com>
2022-04-17 21:08:50 +0300

    Linked list implementation

--- list.cpp
+++ list.cpp
@@ -1,93 +1,100 @@
-/*
-    linked list pointer implementation using head and tail
-    
-    Written by - Mikiyas Worash & Mihret Teshale
-*/
 using namespace std;
 //---------------------------------->List Class definition<-------------------------------------------------//
 
 template <class T>
-struct node {
+struct node
+{
     T data;
-    node<T> * next;
+    node<T> *next;
 };
 
 template <class T>
-class List {
-    public:
-        List(); //default constructor
-        List(List<T> &L); //copy constructor
-        virtual ~List(); //destructor
-        bool isEmpty(){return head == nullptr;}
-        void insert(T data);//insert always by tail
-        void remove(T data);
-        node<T>* find(T data);
-        void empty();
-        //virtual DList<T> * copy();
-        void display();
-        int getCount() {return count;}
-        void copy(List<T> & dst);
-        node<T> * getHead() {return head;}
-    protected:
-        node<T> * head;
-        node<T> * tail;
-        int count;
-    private:
-        bool nodeToDelete(T data, node<T>* &prev, node<T>* &cur);
+class List
+{
+public:
+    List();           //default constructor
+    List(List<T> &L); //copy constructor
+    virtual ~List();  //destructor
+    bool isEmpty() { return head == nullptr; }
+    void insert(T data); //insert always by tail
+    void remove(T data);
+    node<T> *find(T data);
+    void empty();
+    //virtual DList<T> * copy();
+    void display();
+    int getCount() { return count; }
+    void copy(List<T> &dst);
+    node<T> *getHead() { return head; }
 
+protected:
+    node<T> *head;
+    node<T> *tail;
+    int count;
 
+private:
+    bool nodeToDelete(T data, node<T> *&prev, node<T> *&cur);
 };
 
 //------------------------------------>List Class implementation<----------------------------------------//
 
 template <class T>
-List<T>::List() {
+List<T>::List()
+{
     head = nullptr;
     tail = nullptr;
     count = 0;
 }
 
 template <class T>
-List<T>::~List() {
+List<T>::~List()
+{
     empty();
 }
 
 template <class T>
-List<T>::List(List<T> &src) {
-    node<T> * cur = src.head;
+List<T>::List(List<T> &src)
+{
+    node<T> *cur = src.head;
     head = nullptr;
     tail = nullptr;
     count = 0;
-    while (cur != nullptr) {
+    while (cur != nullptr)
+    {
         insert(cur->data);
         cur = cur->next;
     }
 }
 
 template <class T>
-void List<T>::copy(List<T> &dst) {
-    node<T> * cur = head;
+void List<T>::copy(List<T> &dst)
+{
+    node<T> *cur = head;
     dst.head = nullptr;
     dst.tail = nullptr;
     count = 0;
-    while (cur != nullptr) {
+    while (cur != nullptr)
+    {
         dst.insert(cur->data);
         cur = cur->next;
     }
-
 }
 
 template <class T>
-void List<T>:: insert(T data) {
-    node<T>* p = new node<T>;
-    if (p != nullptr) {
-        p -> data = data;
-        p -> next = nullptr;
-        if(head == nullptr) {
+void List<T>::insert(T data)
+{
+    node<T> *p = new node<T>;
+    if (p != nullptr)
+    {
+        p->data = data;
+        p->next = nullptr;
+        if (head == nullptr)
+        {
             head = p;
             tail = p;
             count++;
-        } else {
+        }
+        else
+        {
             tail->next = p;
             tail = p;
             count++;
@@ -96,23 +103,29 @@ void List<T>:: insert(T data) {
 }
 
 template <class T>
-void List<T>::display() {
-    node<T>* cur = head;
-    if (cur == nullptr) {
-        cout<<"List is Empty!";
+void List<T>::display()
+{
+    node<T> *cur = head;
+    if (cur == nullptr)
+    {
+        cout << "List is Empty!";
     }
-    while (cur != nullptr) {
+    while (cur != nullptr)
+    {
         cout << cur->data << "  ";
         cur = cur->next;
     }
 }
 
 template <class T>
-bool List<T>::nodeToDelete(T data, node<T>* &prev, node<T>* &cur) {
+bool List<T>::nodeToDelete(T data, node<T> *&prev, node<T> *&cur)
+{
     cur = head;
     prev = nullptr;
-    while(cur != nullptr) {
-        if (cur->data == data) {
+    while (cur != nullptr)
+    {
+        if (cur->data == data)
+        {
             return true;
         }
         prev = cur;
@@ -122,10 +135,12 @@ bool List<T>::nodeToDelete(T data, node<T>* &prev, node<T>* &cur) {
 }
 
 template <class T>
-void List<T>:: remove(T data) {
-    node<T>* prev;
-    node<T>* cur;
-    if (nodeToDelete(data, prev, cur)) {
+void List<T>::remove(T data)
+{
+    node<T> *prev;
+    node<T> *cur;
+    if (nodeToDelete(data, prev, cur))
+    {
         prev->next = cur->next;
         delete cur;
         count--;
@@ -133,10 +148,13 @@ void List<T>:: remove(T data) {
 }
 
 template <class T>
-node<T>* List<T>:: find(T data) {
-    node<T>* cur = head;
-    while(cur != nullptr) {
-        if(cur->data == data) {
+node<T> *List<T>::find(T data)
+{
+    node<T> *cur = head;
+    while (cur != nullptr)
+    {
+        if (cur->data == data)
+        {
             return cur;
         }
         cur = cur->next;
@@ -145,9 +163,11 @@ node<T>* List<T>:: find(T data) {
 }
 
 template <class T>
-void List<T>:: empty() {
-    node<T> * p;
-    while(head != nullptr){
+void List<T>::empty()
+{
+    node<T> *p;
+    while (head != nullptr)
+    {
         p = head;
         head = head->next;
         delete p;
@@ -155,5 +175,3 @@ void List<T>:: empty() {
     count = 0;
     tail = nullptr;
 }
-
-
--- listTest.cpp
+++ listTest.cpp
@@ -1,12 +1,8 @@
-/*
-    Testing method for testing list
-
-    Written by - Mikiyas Worash & Mihret Teshale
-*/
 #include <iostream>
 using namespace std;
 #include "list.cpp"
-int main() {
+int main()
+{
     List<int> list;
     list.insert(90);
     list.insert(20);
@@ -16,24 +12,25 @@ int main() {
     list.insert(25);
     list.insert(555);
     list.display();
-    cout<<endl<<list.getCount();
-    cout<<endl;
+    cout << endl
+         << list.getCount();
+    cout << endl;
     list.remove(50);
     list.remove(20);
     list.display();
-    cout<<endl<<list.getCount();
-    cout<<endl;
-    cout<<list.isEmpty()<<endl;
-    cout<<endl;
+    cout << endl
+         << list.getCount();
+    cout << endl;
+    cout << list.isEmpty() << endl;
+    cout << endl;
     List<int> list2;
     list.copy(list2);
     list2.display();
     list2.insert(44);
     list2.insert(4899);
     list.insert(1);
-    cout<<endl;
+    cout << endl;
     list2.display();
-    cout<<endl;
+    cout << endl;
     list.display();
-
 }
