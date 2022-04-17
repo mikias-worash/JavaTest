84a057374b8e17d2b054f7b60361482d7b1b7600
Mikias <mikias.worash.a@gmail.com>
2022-04-17 21:09:56 +0300

    Main function

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
-/*
-    Dictionary Application, main method
-
-    Written by - Mikiyas Worash & Mihret Teshale
-    Written for CS551 MiniProject assigned by Mr. Atnafu J
-    Written date - Nov, 2021
-*/
-
 #include <iostream>
 using namespace std;
 #include "dicLib.cpp"
 
-int main() {
+int main()
+{
     menu();
 }
